989998133be2ff45151c88d8d8b2d7b7ac19c67d
sayan-k-d <sayan.sayanak2000@gmail.com>
2023-12-21 03:43:31 +0530

    Street Canvas Prototype:v1

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let underline = document.getElementsByClassName("underline");
+if (window.location.pathname === "/work.html") {
+  document.querySelector(".work").classList.add("current-link");
+} else if (window.location.pathname === "/about.html")
+  document.querySelector(".about").classList.add("current-link");
+else if (window.location.pathname === "/bookings.html")
+  document.querySelector(".booking").classList.add("current-link");
+
+for (let i = 0; i < underline.length; i++)
+  underline[i].classList.add("underline-animate");
+
+function changeBg(bg = "../assets/IMG_0669.JPG") {
+  document.querySelector(".main-image").style.backgroundImage = `url(${bg})`;
+  document.querySelector(".main-image").style.transition =
+    "background-image 0.5s linear";
+}
+
+//bookings
+function getAlert() {
+  Swal.fire({
+    title: "Booking Confirmation",
+    text: "Appoinment Booked Successfully!",
+    icon: "success",
+  });
+}
